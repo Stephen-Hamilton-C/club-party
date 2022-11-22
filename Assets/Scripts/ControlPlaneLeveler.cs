45274c4db3e9084d0748de4aa6c57f9d0ead8267
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-11-22 00:04:03 -0500

    Control plane follows ball Y
    This allows support for hills and ramps

---
+++ Assets/Scripts/ControlPlaneLeveler.cs
@@ -0,0 +1,13 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class ControlPlaneLeveler : MonoBehaviour {
+
+    private void Update() {
+        var character = (PhotonNetwork.LocalPlayer.CustomProperties["Character"] as GameObject)?.transform;
+        if (character != null) {
+            transform.position = new(0, character.position.y - character.localScale.y / 2, 0);
+        }
+    }
+    
+}
