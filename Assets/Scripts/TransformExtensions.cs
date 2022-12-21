070388a25eb5efb42843876a0ca7097663ac9bcc
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-12-21 12:31:19 -0500

    Created a simple GetChildren Transform extension

---
+++ Assets/Scripts/TransformExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class TransformExtensions {
+    /// <summary>
+    /// Gets all immediate children in this transform
+    /// </summary>
+    /// <returns>All immediate child transforms in this transform</returns>
+    public static Transform[] GetChildren(this Transform transform) {
+        var children = new Transform[transform.childCount];
+        var i = 0;
+        foreach (Transform child in transform) {
+            children[i] = child;
+            i++;
+        }
+
+        return children;
+    }
+}
