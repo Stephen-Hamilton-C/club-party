6d3d06c90de5cca82bdb19a07483d188508277a4
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-12-25 01:20:58 -0500

    Fixed score tracking

--- Assets/Scripts/Ball/ScoreTracker.cs
+++ Assets/Scripts/Ball/ScoreTracker.cs
@@ -1,28 +1,34 @@
 using System.Collections.Generic;
+using ExitGames.Client.Photon;
+using Photon.Pun;
 using UnityEngine;
 
 namespace Ball {
+    [RequireComponent(typeof(PhotonView))]
     public class ScoreTracker : MonoBehaviour {
     
         [SerializeField] private bool debug;
         [SerializeField] private int lateJoinScore;
 
-	// TODO: Store this with PhotonPlayer.CustomSettings instead
-        public IReadOnlyList<int> Scores => _scores;
         private int[] _scores;
 
         private Logger _logger;
+        private PhotonView _view;
         private Hole[] Holes => GameManager.Instance.holes;
 	
         private void Start() {
             _logger = new(this, debug);
+            _view = GetComponent<PhotonView>();
+            if(!_view.IsMine)
+                Destroy(this);
             
             _scores = new int[Holes.Length];
             for (int i = 0; i < _scores.Length; i++) {
                 // Set score to lateJoinScore for holes the player never played if they join late
                 _scores[i] = i >= GameManager.HoleIndex ? -Holes[i].Par : lateJoinScore;
             }
-            
+
+            UpdateScores();
             _logger.Log("Scores initialized to "+string.Join(", ", _scores));
 
             LocalPlayerState.OnStroke += PlayerStroked;
@@ -32,8 +38,14 @@ namespace Ball {
             LocalPlayerState.OnStroke -= PlayerStroked;
         }
 
+        private void UpdateScores() {
+            var hashtable = new Hashtable() { { "Scores", _scores } };
+            PhotonNetwork.LocalPlayer.SetCustomProperties(hashtable);
+        }
+
         private void PlayerStroked() {
             _scores[GameManager.HoleIndex]++;
+            UpdateScores();
             _logger.Log("Scores set to "+string.Join(", ", _scores));
         }
 
