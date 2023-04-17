de773a7e419907f2e2b11c87f483d68e188a1057
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2023-04-17 16:06:51 -0400

    Created editor script to auto-deploy WebGL build to server

---
+++ Assets/Editor/SHamilton/ClubParty/AutoDeploy.cs
@@ -0,0 +1,48 @@
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using System.IO;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+namespace SHamilton.ClubParty {
+    public static class AutoDeploy {
+        [PostProcessBuild(100)]
+        public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
+            if (target != BuildTarget.WebGL) return;
+            
+            // Check if Deploy config files are present before asking this
+            var assets = Application.dataPath; // Project Folder/Assets
+            var deployDir = Path.Join(assets, "Deploy");
+            var deployConfigPath = Path.Join(deployDir, "deploy-config.txt");
+            if (!File.Exists(deployConfigPath)) {
+                Debug.LogWarning("No deploy.py config files found! " +
+                                 "You should run 'python deploy.py' in the Deploy folder " +
+                                 "to go through the first-run setup.");
+                return;
+            }
+            
+            var shouldDeploy = EditorUtility.DisplayDialog(
+                "Deploy WebGL Build?",
+                "Do you want to deploy this WebGL build to the server?",
+                "Yes",
+                "No"
+            );
+            if (shouldDeploy) {
+                // TODO: Maybe try a separate thread? The editor appears to hang when yes is clicked.
+                // TODO: Test this with Windows
+                Debug.LogWarning("NOT IMPLEMENTED: Need to run deploy.py");
+                var deployScript = Path.Join(deployDir, "deploy.py");
+                var process = new Process();
+                process.StartInfo = new ProcessStartInfo("python", deployScript) {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = false,
+                };
+                process.Start();
+                var output = process.StandardOutput.ReadToEnd();
+                Debug.Log("deploy.py output:\n"+output);
+            }
+        }
+    }
+}
